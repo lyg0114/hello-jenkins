131188160c1c68beee73d24072c31dc68c6cd76f
lyg0114 <whdnseowkd@naver.com>
2023-04-21 17:28:03 +0900

    first commit

---
+++ src/test/java/com/example/hellojenkins/HelloJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellojenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloJenkinsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
